2b39bc282d35770b71efacadf3a59d32b1fb7a8e
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-08 21:20:59 +0200

    Refactor

--- test/voting.js
+++ test/voting.js
@@ -2,17 +2,31 @@ const Voting = artifacts.require('Voting');
 const { time } = require('@openzeppelin/test-helpers');
 
 contract('Voting', accounts => {
+  this.electionName = web3.utils.toHex('test election')
+  this.electionProposal = 'Test proposal'
+  this.registrationDeadline = 140
+  this.votingDeadline = 280
+  this.endingTime = 560
+
   const deploy = async () => {
     instance = await Voting.new();
-    registrationDeadline = 140, votingDeadline = 280, endingTime = 560;
+  }
+
+  const createElection = async (args) => {
+    args = args || {}
+    const en = args.electionName || this.electionName
+    const ep = args.electionProposal || this.electionProposal
+    const rd = args.registrationDeadline || this.registrationDeadline
+    const vd = args.votingDeadline || this.votingDeadline
+    const et = args.endingTime || this.endingTime
+
+    await instance.newElection(en, ep, rd, vd, et, { from: accounts[0] })
   }
 
   describe('newElection', async () => {
     beforeEach(async () => {
       await deploy()
-      electionName = web3.utils.toHex('test election');
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
-
+      await createElection();
       originalBlock = await time.latest()
     });
 
@@ -23,22 +37,25 @@ contract('Voting', accounts => {
 
     it('sets registrationDeadline relative to current block timestamp', async () => {
       const election = await instance.elections(0);
-      assert.strictEqual(election.registrationDeadline.toNumber(), originalBlock.toNumber() + registrationDeadline);
+      const registrationDeadline = await election.registrationDeadline.toNumber();
+      assert.equal(registrationDeadline, originalBlock.toNumber() + this.registrationDeadline);
     });
 
     it('sets votingDeadline relative to current block timestamp', async () => {
       const election = await instance.elections(0);
-      assert.strictEqual(election.votingDeadline.toNumber(), originalBlock.toNumber() + votingDeadline);
+      const votingDeadline = election.votingDeadline.toNumber()
+      assert.strictEqual(votingDeadline, originalBlock.toNumber() + this.votingDeadline);
     });
 
     it('sets endingTime relative to current block timestamp', async () => {
       const election = await instance.elections(0);
-      assert.strictEqual(election.endingTime.toNumber(), originalBlock.toNumber() + endingTime);
+      const endingTime = election.endingTime.toNumber()
+      assert.strictEqual(endingTime, originalBlock.toNumber() + this.endingTime);
     });
 
     it('should NOT create an election if name is already taken', async () => {
       try {
-        await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
+        await createElection()
       } catch (error) {
         err = error
       }
@@ -51,18 +68,17 @@ contract('Voting', accounts => {
   describe('registerCandidate', async () => {
     beforeEach(async () => {
       await deploy()
-      electionName = web3.utils.toHex('test election');
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
+      await createElection()
     })
 
     it('registers a candidate to a given election', async () => {
       const candidateName = web3.utils.toHex('Bob');
-      await instance.registerCandidate(electionName, candidateName, { from: accounts[0] });
+      await instance.registerCandidate(this.electionName, candidateName, { from: accounts[0] });
       const election = await instance.elections(0);
       const candidate = await instance.candidates(accounts[0]);
 
       assert.isOk(candidate);
-      assert.strictEqual(web3.utils.hexToUtf8(candidate.electionName), web3.utils.hexToUtf8(electionName));
+      assert.strictEqual(web3.utils.hexToUtf8(candidate.electionName), web3.utils.hexToUtf8(this.electionName));
     });
 
     it('errors if the election does not exist', async () => {
@@ -80,12 +96,12 @@ contract('Voting', accounts => {
     });
 
     it('errors if the registration period has ended', async () => {
-      await time.increase(registrationDeadline);
+      await time.increase(this.registrationDeadline);
 
       const candidateName = web3.utils.toHex('Bob');
 
       try {
-        await instance.registerCandidate(electionName, candidateName, { from: accounts[0] });
+        await instance.registerCandidate(this.electionName, candidateName, { from: accounts[0] });
       } catch(error) {
         err = error
       };
@@ -98,7 +114,7 @@ contract('Voting', accounts => {
       const electionName = web3.utils.toHex('other test');
       const candidateName = web3.utils.toHex('bob');
 
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
+      await createElection({electionName: electionName})
       await instance.registerCandidate(electionName, candidateName, { from: accounts[0] });
 
       try {
@@ -115,16 +131,14 @@ contract('Voting', accounts => {
   describe('getElectionCandidates', () => {
     beforeEach(async () => {
       await deploy()
-      electionName = web3.utils.toHex('test election');
-      const [candidate1, candidate2, candidate3] = accounts
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
-      await instance.registerCandidate(electionName, web3.utils.toHex('Candidate 1'), { from: candidate1 })
-      await instance.registerCandidate(electionName, web3.utils.toHex('Candidate 2'), { from: candidate2 })
-      await instance.registerCandidate(electionName, web3.utils.toHex('Candidate 3'), { from: candidate3 })
+      await createElection()
+      await instance.registerCandidate(this.electionName, web3.utils.toHex('Candidate 1'), { from: accounts[0] })
+      await instance.registerCandidate(this.electionName, web3.utils.toHex('Candidate 2'), { from: accounts[1] })
+      await instance.registerCandidate(this.electionName, web3.utils.toHex('Candidate 3'), { from: accounts[2] })
     })
 
     it('returns an array containing all the candidates', async () => {
-      const candidates = await instance.getElectionCandidates(electionName)
+      const candidates = await instance.getElectionCandidates(this.electionName)
       assert(candidates.length >= 3)
     })
   })
@@ -132,17 +146,15 @@ contract('Voting', accounts => {
   describe('removeCandidate', () => {
     beforeEach(async () => {
       await deploy()
-      electionName = web3.utils.toHex('test election');
-      candidateName = web3.utils.toHex('Bob');
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
+      await createElection()
     })
 
     it('removes a candidate from a given election', async () => {
       const candidateAddress = accounts[0]
-      const nonCandidateAddress = accounts[1]
+      const candidateName = web3.utils.toHex('Candidate 1')
 
-      await instance.registerCandidate(electionName, candidateName, { from: accounts[0] });
-      await instance.removeCandidate(electionName, candidateAddress)
+      await instance.registerCandidate(this.electionName, candidateName, { from: candidateAddress });
+      await instance.removeCandidate(this.electionName, candidateAddress)
 
       const candidate = await instance.candidates(candidateAddress)
 
@@ -151,10 +163,13 @@ contract('Voting', accounts => {
 
     it('errors if another account tries to remove a candidate', async () => {
       const candidateAddress = accounts[0]
+      const candidateName = web3.utils.toHex('Candidate 1')
       const nonCandidateAddress = accounts[1]
 
+      await instance.registerCandidate(this.electionName, candidateName, { from: candidateAddress });
+
       try {
-        await instance.removeCandidate(electionName, nonCandidateAddress)
+        await instance.removeCandidate(this.electionName, nonCandidateAddress)
       } catch(error) {
         err = error
       }
@@ -164,10 +179,10 @@ contract('Voting', accounts => {
     })
 
     it('errors if that candidate is not found in the election', async () => {
-      const candidateAddress = accounts[3]
+      const candidateAddress = accounts[0]
 
       try {
-        await instance.removeCandidate(electionName, candidateAddress, { from: candidateAddress })
+        await instance.removeCandidate(this.electionName, candidateAddress, { from: candidateAddress })
       } catch(error) {
         err = error
       }
@@ -180,45 +195,41 @@ contract('Voting', accounts => {
   describe('vote', () => {
     beforeEach(async () => {
       await deploy()
-      electionName = web3.utils.toHex('test election');
-      candidateName1 = web3.utils.toHex('Bob');
-      candidateName2 = web3.utils.toHex('Bob');
-      candidateName3 = web3.utils.toHex('Bob');
+      const candidateName1 = web3.utils.toHex('Bob');
+      const candidateName2 = web3.utils.toHex('Bob');
+      const candidateName3 = web3.utils.toHex('Bob');
 
       [candidate1, candidate2, candidate3, voter1, voter2, voter3, voter4] = accounts
-      await instance.newElection(electionName, registrationDeadline, votingDeadline, endingTime, { from: accounts[0] });
+      await createElection()
 
-      await instance.registerCandidate(electionName, candidateName1, { from: candidate1 })
-      await instance.registerCandidate(electionName, candidateName2, { from: candidate2 })
-      await instance.registerCandidate(electionName, candidateName3, { from: candidate3 })
+      await instance.registerCandidate(this.electionName, candidateName1, { from: candidate1 })
+      await instance.registerCandidate(this.electionName, candidateName2, { from: candidate2 })
+      await instance.registerCandidate(this.electionName, candidateName3, { from: candidate3 })
     })
 
     it('registers a vote', async () => {
-      const vote = await instance.vote(electionName, candidate1, { from: voter1 })
+      const vote = await instance.vote(this.electionName, candidate1, { from: voter1 })
       assert.isOk(vote)
     })
 
     it('bumps the candidate\'s vote count by 1 for every vote registered', async () => {
-      let candidate;
-      candidate = await instance.candidates(candidate1)
-      assert.equal(candidate.votes.toNumber(), 0)
+      const assertVote = async (candidate, voter, expectedResult) => {
+        await instance.vote(this.electionName, candidate, { from: voter })
+        let candidatee = await instance.candidates(candidate)
+        assert.equal(candidatee.votes.toNumber(), expectedResult)
+      }
 
-      await instance.vote(electionName, candidate1, { from: voter1 })
-
-      candidate = await instance.candidates(candidate1)
-      assert.equal(candidate.votes.toNumber(), 1)
-
-      await instance.vote(electionName, candidate1, { from: voter2 })
-
-      candidate = await instance.candidates(candidate1)
-      assert.equal(candidate.votes.toNumber(), 2)
+      await assertVote(candidate1, voter1, 1)
+      await assertVote(candidate1, voter2, 2)
+      await assertVote(candidate1, voter3, 3)
+      await assertVote(candidate1, voter4, 4)
     })
 
     it('errors if election has already ended', async () => {
-      await time.increase(endingTime)
+      await time.increase(this.endingTime)
 
       try {
-        await instance.vote(electionName, candidate1, { from: voter1 })
+        await instance.vote(this.electionName, candidate1, { from: voter1 })
       } catch(error) {
         err = error
       }
@@ -228,10 +239,10 @@ contract('Voting', accounts => {
     })
 
     it('errors if voting period is over', async () => {
-      await time.increase(votingDeadline)
+      await time.increase(this.votingDeadline)
 
       try {
-        await instance.vote(electionName, candidate1, { from: voter1 })
+        await instance.vote(this.electionName, candidate1, { from: voter1 })
       } catch(error) {
         err = error
       }
@@ -241,10 +252,10 @@ contract('Voting', accounts => {
     })
 
     it('errors if registration period is still happening', async () => {
-      await time.increase(registrationDeadline + 1)
+      await time.increase(this.registrationDeadline + 1)
 
       try {
-        await instance.vote(electionName, candidate1, { from: voter1 })
+        await instance.vote(this.electionName, candidate1, { from: voter1 })
       } catch(error) {
         err = error
       }
