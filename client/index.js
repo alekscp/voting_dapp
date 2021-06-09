e34d9478de3ea605ba877b8f46cd8edae177c033
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-09 12:06:03 +0200

    Initialize ethereum provider

--- client/index.js
+++ client/index.js
@@ -3,3 +3,32 @@ import Voting from '../build/contracts/Voting.json';
 
 let web3;
 let voting;
+
+const initWeb3 = () => {
+  return new Promise((resolve, reject) => {
+    if(typeof window.ethereum !== 'undefined') {
+      // const web3 = new Web3(window.ethereum);
+      window.ethereum.enable()
+        .then(() => {
+          resolve(new Web3(window.ethereum));
+        })
+        .catch(e => {
+          reject(e);
+        })
+      return;
+    }
+    if(typeof window.web3 !== 'undefined') {
+      return resolve(
+        new Web3(window.web3.currentProvider)
+      );
+    }
+    resolve(new Web3('http://localhost:9545'));
+  })
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initWeb3()
+    .then(_web3 => {
+      web3 = _web3;
+    })
+});
